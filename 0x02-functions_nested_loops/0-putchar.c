05a49af2766c53b27e7160b6026c0118b26d304a
Mohammed Ishaw <mohaishaw@gmail.com>
2022-09-13 13:57:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,9 @@
 /**                                                                                                                       
  * main - print putchar                                                                                                   
  * Return: always 0                                                                                                       
- */                                                                                                                       int main(void)                                                                                                            
+ */
+
+int main(void)                                                                                                            
 {
        	_putchar('_');                                                                                                    
         _putchar('p');
@@ -16,4 +18,5 @@
         _putchar('\n');                                                                                                   
                                                                                                                           
         return 0;                                                                                                         
+
 }
